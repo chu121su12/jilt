fc45e6579cce9bc58e9488a2c0d5c4de2ad87beb
Adam Ruka <adamruka85@gmail.com>
2024-06-08 09:11:34 -0700

    Introduce a Functional example of User.

--- src/test/java/org/jilt/test/data/functional/LargeLanguageModel.java
+++ src/test/java/org/jilt/test/data/functional/LargeLanguageModel.java
@@ -2,17 +2,16 @@ package org.jilt.test.data.functional;
 
 import org.jilt.Builder;
 import org.jilt.BuilderStyle;
-import org.jilt.Opt;
 
 @Builder(style = BuilderStyle.FUNCTIONAL, toBuilder = "toBuilder", buildMethod = "create")
 public final class LargeLanguageModel {
     public final String name;
-    @Opt public final Float temperature;
-    @Opt public final Integer outputTokensLimit;
+    public final float temperature;
+    public final int outputTokensLimit;
 
-    public LargeLanguageModel(String name, Float temperature, Integer outputTokensLimit) {
+    public LargeLanguageModel(String name, float temperature, int outputTokensLimit) {
         this.name = name;
-        this.temperature =  temperature == null ? 0.3F : temperature;
-        this.outputTokensLimit = outputTokensLimit == null ? 100 : outputTokensLimit;
+        this.temperature =  temperature;
+        this.outputTokensLimit = outputTokensLimit;
     }
 }
